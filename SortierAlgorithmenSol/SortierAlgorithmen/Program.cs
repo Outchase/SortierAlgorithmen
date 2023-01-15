a58580f37dfb1010f9a125efed8de7ff74104db6
Outchase <101480664+Outchase@users.noreply.github.com>
2023-01-15 21:50:05 +0100

    Input user

--- SortierAlgorithmenSol/SortierAlgorithmen/Program.cs
+++ SortierAlgorithmenSol/SortierAlgorithmen/Program.cs
@@ -7,16 +7,27 @@ namespace SortierAlgorithmen
         static void Main(string[] args)
         {
             bool wantToSortAgain = true;
+            bool isNumber = false;
+            int numberOfNumbers = 0;
             ASCIISIGN sign = new();
             Tools tools = new();
+
 
             while (wantToSortAgain)
             {
                 tools.PrintInColor(sign.title + "\n" + "Press Enter to Start\n", ConsoleColor.Yellow, true);
                 tools.OnPressContinue(true);
 
-                tools.PrintInColor("Please enter the amount of number you want to sort: ", ConsoleColor.Green, false);
-                Console.ReadLine();
+                while(!isNumber)
+                {
+                    tools.PrintInColor("Please enter the amount of number you want to sort: ", ConsoleColor.Green, false);
+                    isNumber = int.TryParse(Console.ReadLine(), out numberOfNumbers);
+                }
+
+
+
+                Console.WriteLine(isNumber);
+                Console.WriteLine(numberOfNumbers);
 
                 Console.ReadKey();
                 wantToSortAgain= false;
