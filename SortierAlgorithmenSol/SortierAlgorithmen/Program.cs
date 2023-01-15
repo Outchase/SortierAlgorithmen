f6bae79c85cc27422345bb06796c24b77c679949
Outchase <101480664+Outchase@users.noreply.github.com>
2023-01-15 03:15:15 +0100

    Intro and Outro done

--- SortierAlgorithmenSol/SortierAlgorithmen/Program.cs
+++ SortierAlgorithmenSol/SortierAlgorithmen/Program.cs
@@ -6,7 +6,25 @@ namespace SortierAlgorithmen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool wantToSortAgain = true;
+            ASCIISIGN sign = new();
+            Tools tools = new();
+
+            while (wantToSortAgain)
+            {
+                tools.PrintInColor(sign.title + "\n" + "Press Enter to Start\n", ConsoleColor.Yellow, true);
+                tools.OnPressContinue(true);
+
+                tools.PrintInColor("Please enter the amount of number you want to sort: ", ConsoleColor.Green, false);
+                Console.ReadLine();
+
+                Console.ReadKey();
+                wantToSortAgain= false;
+            }
+
+            tools.PrintInColor(sign.outro, ConsoleColor.Yellow, true);
+
+            Environment.Exit(0);
         }
     }
 }
---
+++ SortierAlgorithmenSol/SortierAlgorithmen/Tools.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortierAlgorithmen
+{
+    internal class Tools
+    {
+        public void PrintInColor(string message, ConsoleColor color, bool resetColor)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            if (resetColor)
+            {
+                Console.ResetColor();
+            }
+        }
+
+        //Listens to enter key press, if parameter is true it clears the console
+        public void OnPressContinue(bool clearConsole)
+        {
+            ConsoleKeyInfo userKeyInput;
+            bool keyPressedRight = false;
+
+            while (!keyPressedRight)
+            {
+
+                userKeyInput = Console.ReadKey(true);
+
+                if (userKeyInput.Key == ConsoleKey.Enter)
+                {
+                    keyPressedRight = true;
+                }
+            }
+
+            if (clearConsole)
+            {
+                Console.Clear();
+            }
+        }
+
+        //Listens to two key press and return value, if parameter is true it clears the console
+        public bool OnPressYesOrNo(bool clearConsole)
+        {
+            ConsoleKeyInfo userKeyInput;
+            bool keyPressedRight = false;
+            bool isAYes = false;
+
+            while (!keyPressedRight)
+            {
+
+                userKeyInput = Console.ReadKey(true);
+
+                if (userKeyInput.Key == ConsoleKey.Y)
+                {
+                    keyPressedRight = true;
+                    isAYes = true;
+                }
+                else if (userKeyInput.Key == ConsoleKey.N)
+                {
+                    keyPressedRight = true;
+                }
+            }
+            if (clearConsole)
+            {
+                Console.Clear();
+            }
+            return isAYes;
+
+        }
+
+    }
+}
